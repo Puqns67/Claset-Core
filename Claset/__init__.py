749f81cf022907e0b908ee1172c0ef2d6feaa53a
Puqns67 <me@puqns67.icu>
2022-03-11 01:18:55 +0800

    更新: 更新构建号, 提交 template.pot

--- Claset/__init__.py
+++ Claset/__init__.py
@@ -7,7 +7,7 @@
 __author__ = "Puqns67"
 __productname__ = "Claset"
 __version__ = "0.1.0"
-__build__ = 156
+__build__ = 158
 __fullversion__ = __version__ + "_" + str(__build__)
 __all__ = (
     "Accounts", "Execution", "Utils", "Game",
